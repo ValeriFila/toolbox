f8439d9bcb4ab312b2949ea3dead5c145e742868
Valeria <8918valeria@gmail.com>
2024-02-26 15:40:33 +0300

    add notes page and styles

---
+++ src/components/Note/Note.jsx
@@ -0,0 +1,43 @@
+import classes from './Note.module.scss'
+
+const Note = ({ props }) => {
+    return (
+        <div className={classes.note}>
+            {props.note ? (
+                <p className={classes.note__text}>{props.note}</p>
+            ) : (
+                <textarea
+                    placeholder='Новая заметка...'
+                    className={classes.note__text}
+                >
+                    {props.textarea}
+                </textarea>
+            )}
+            <div className={classes.note__bottom}>
+                {props.date ? (
+                    <p className={classes.note__bottom__date}>{props.date}</p>
+                ) : (
+                    <p className={classes.note__bottom__date}>{props.remaining}</p>
+                )}
+
+                {props.button ? (
+                    <button
+                        type='button'
+                        className={classes.note__bottom__button}
+                    >
+                        {props.button}
+                    </button>
+                ) : (
+                    <button
+                        type='button'
+                        className={classes.note__bottom__button}
+                    >
+                        удалить
+                    </button>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Note
---
+++ src/pages/Notes/Notes.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Note from '../../components/Note/Note'
+import './Notes.scss'
+
+const Notes = () => {
+    return (
+        <div className='notes-page'>
+            <h1 className='notes-page__title'>Заметки</h1>
+            <div className='notes-page__notes-section'>
+                <Note
+                    props={{
+                        note: 'Купить хлеб в магните. ТАкже сходить в зал, позвонить маме и сделать йогу',
+                        date: '26.02.2024',
+                    }}
+                />
+                <Note
+                    props={{
+                        note: 'Купить хлеб в магните. ТАкже сходить в зал, позвонить маме и сделать йогу',
+                        date: '26.02.2024',
+                    }}
+                />
+                <Note
+                    props={{
+                        note: 'Купить хлеб в магните. ТАкже сходить в зал, позвонить маме и сделать йогу',
+                        date: '26.02.2024',
+                    }}
+                />
+                <Note
+                    props={{
+                        textarea: '',
+                        remaining: '198 символов',
+                        button: 'Создать',
+                    }}
+                />
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Notes
