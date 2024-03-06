74a4b318045ed33ec42f96c45413e885ad8cd3f4
Valeria <8918valeria@gmail.com>
2024-03-06 10:15:12 +0300

    add city searching and styles

--- src/features/components/CurrentWeather/components/CurrentWeather.jsx
+++ src/features/components/CurrentWeather/components/CurrentWeather.jsx
@@ -1,37 +1,68 @@
 import './CurrentWeather.scss'
-import { useRef } from 'react'
+import { useMemo, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useGetCurrentWeatherQuery } from '../api/weatherApi.js'
 import { useCurrentPosition } from '../lib/helpers/useCurrentPosition'
 import { WeatherBlock } from '../../../../entities/Weather'
+import { useFetchCityMutation } from '../api/cityApi.js'
 
 export const CurrentWeather = () => {
+    const [queryRes, setQueryRes] = useState(null)
     const location = useSelector((state) => state.location.location)
-    const ref = useRef()
+    const ref = useRef('')
 
     useCurrentPosition()
 
-    const handleChange = () => {
-        console.log('change')
-    }
-
-    const fetchTheWeather = () => {
-        console.log('fetch the weather...')
-        console.log(location)
-    }
-
+    const [fetchCity, result] = useFetchCityMutation()
     const {
         data,
         // isFetching,
         // isLoading,
     } = useGetCurrentWeatherQuery(location)
+    const fetchTheWeather = () => {
+        console.log('fetch the weather...')
+        console.log(location)
+    }
+    const handleChange = (e) => {
+        ref.current.value = e.target.value.trim()
+        fetchCity(ref.current.value)
+        if (e.target.value.trim().length === 0) {
+            setQueryRes(null)
+        } else {
+            setQueryRes(result.data)
+        }
+    }
+
+    const createdCitiesList = useMemo(() => {
+        if (!queryRes) {
+            return (
+                <p>Такого города не найдено</p>
+            )
+        } 
+        const citiesArray = queryRes?.suggestions.reduce((accum, city) => {
+            return [...accum, city.data.city]
+        }, [])
+        const uniqueCities = new Set([...citiesArray])
+        const uniqueCitiesArray = [...uniqueCities]
+
+        return uniqueCitiesArray.map((city) => {
+            return (
+                <p className='current-weather__cities__city'>{city}</p>
+            )
+        })
+    }, [queryRes])
 
     return (
-        <WeatherBlock
-            data={data}
-            inputRef={ref}
-            onChange={(e) => handleChange(e)}
-            onClick={() => fetchTheWeather()}
-        />
+        <div className='current-weather'>
+            <WeatherBlock
+                data={data}
+                inputRef={ref}
+                onChange={(e) => handleChange(e)}
+                onClick={() => fetchTheWeather()}
+            />
+            <div className='current-weather__cities'>
+                {createdCitiesList}
+            </div>
+        </div>
     )
 }
--- src/shared/api/rtkApi/rtkApi.js
+++ src/shared/api/rtkApi/rtkApi.js
@@ -1,9 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const API_KEY = '8NPlFd4xqrDO+pje2HGRIA==aR7Xwz9LmSSRrQJ4'
+
 export const rtkApi = createApi({
     reducerPath: 'weather',
     baseQuery: fetchBaseQuery({
         // baseUrl: 'http://api.weatherapi.com/v1',
+        // prepareHeaders: {
+        //     'X-Api-Key': API_KEY,
+        //     contentType: 'application/json',
+        // },
     }),
     tagTypes: [
         'Weather',
